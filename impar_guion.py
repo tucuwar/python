4577e256aeb37cc3d3806ab74e07722caa5b7f10
tucuwar <tucuwar@gmail.com>
2018-05-21 12:46:17 -0300

    idea general

---
+++ impar_guion.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#solution('abc') # should return ['ab', 'c_']
+#solution('abcdef') # should return ['ab', 'cd', 'ef']
+
+def solution(s):
+    tam=len(s)
+    P=[]
+    I=[]
+    if (tam % 2 == 0):
+        print "PAR"
+        for i in s:
+            P = 
+            
+
+    else:
+        print "IMPAR"
+        #agrego _ para completar
+
+s = solution(raw_input("Ingresa string: "))

65954ee9f764886ee7a3e470c3567bd8e92af9fe
tucuwar <tucuwar@gmail.com>
2018-05-21 10:48:07 -0300

    funcionando

--- reverse_word.py
+++ reverse_word.py
@@ -4,25 +4,15 @@
 # "double  spaces"      ==> "elbuod  secaps"
 
 def reverse_words(cad):
-    C = cad.split(' ')
-    
-    pivot = [] #guardo palabra al reves
-    for i in C: # pasa primera palabra i
-        tam = len(i)
-        print tam
-        word = []
-        reverse = []
-        for x in i:
-            word.append(x)
-        print word
-        #cont = tam
-        for l in range(0,tam,1):
-            reverse.append(word[l]) #debe ir de mayor a menor
-        print reverse
-      
-    pass
-
-
-
+    vere = [] 
+    s = cad.split(" ") #string a lista. Separo por espacio en blanco
+    for i in s:
+        a = i[::-1] #al reves cada palabra
+        vere.append(a) #voy guardando en lista
+    b = " ".join(vere) #lista a string
+    return b
+    print b
+       
 cad = raw_input("Ingrese cadena de texto: ")
-reverse_words(cad)
+b = reverse_words(cad)
+print b

930bfffc3ec0025caa4b0c7eb41e013ef7dc2f02
tucuwar <tucuwar@gmail.com>
2018-05-23 18:26:41 -0300

    funcionando OK

--- impar_guion.py
+++ impar_guion.py
@@ -7,14 +7,26 @@ def solution(s):
     tam=len(s)
     P=[]
     I=[]
+    ii = 0
+    f = 2
     if (tam % 2 == 0):
-        print "PAR"
-        for i in s:
-            P = 
-            
-
+        #print "PAR"
+        for i in range(len(s)/2): #range(0,tam/2,1):
+            i = s[ii:f:1]
+            P.append(i)
+            ii += 2
+            f += 2
+        print P
     else:
-        print "IMPAR"
-        #agrego _ para completar
+        #print "IMPAR"
+        for i in range(len(s)/2+1): #range(0,tam/2+1,1):
+            i = s[ii:f:1]
+            I.append(i)
+            ii += 2 
+            f += 2
+        #print I[-1] #ultimo elemento de una lista 
+        I[-1] = I[-1]+"_"
+        print I
+        
 
 s = solution(raw_input("Ingresa string: "))

a3bec8938cb911ef57e3cf02576d70720a8269a8
tucuwar <tucuwar@gmail.com>
2018-08-07 12:46:20 -0300

    Nuevo version . con errores

--- dolar/dolar.py
+++ dolar/dolar.py
@@ -2,6 +2,9 @@
 
 ### Seguimiento de los cambios del dolar 
 
+#webs referencia
+# https://python-para-impacientes.blogspot.com/2014/08/graficos-en-ipython.html
+
 #Librerias:
 # pip install matplotlib
 
@@ -15,21 +18,46 @@ from pylab import *
 # y NumPy con los alias mpl y np, respetivamente.
 
 
+#fecha = raw_input("Ingrese semana: dd al dd de MM/AAAA")
 
-dolar_compra = [28.3, 28.2, 28.6, 28.8, 28.7]
-dolar_venta = [26.1, 25.8, 26.9, 27.1, 27.2]
 
-#dolar_dia = [L, M, Mi, J, V]
-  
-#plt.title("Título")    # Establece el título del gráfico
-#plt.xlabel("abscisa")  # Establece el título del eje x 
-#plt.ylabel("ordenada") # Establece el título del eje y
+#dolar_compra = [28.3, 28.2, 28.6, 28.8, 28.7]
+#dolar_venta = [26.1, 25.8, 26.9, 27.1, 27.2]
 
-#plt.plot(dolar_compra)  # Dibuja el gráfico
-#plt.plot(dolar_venta)  # Dibuja el gráfico
+dolar_compra = []
+dolar_venta = []
+dolar_dia = ["Lun", "Mar", "Mie", "Jue", "Vie"]
 
-plt.plot(dolar_compra, label = "Dolar Comprador")
-plt.plot(dolar_venta, label = "Dolar Vendedor")
+
+
+plt.title(raw_input("Fecha: "))     # Establece el título del gráfico   
+print("Ingrese los valores del Dolar en la semana establecida")
+
+for d in dolar_dia:
+    print("Valor Dolar día %s. ")%(d)
+    c = float(raw_input("Comprador: "))
+    v = float(raw_input("Vendedor: "))
+    dolar_compra.append(c)
+    dolar_venta.append(v)
+    print (" ")
+
+
+#plt.xlabel("Valor dolar")          # Establece el título del eje x = abscisa
+#plt.ylabel("Días de la semana")    # Establece el título del eje y = ordenada
+
+print("Comprador",dolar_compra)
+print("Vendedor",dolar_venta)
+
+raw_input("Pulsa una tecla para continuar...")
+
+plt.plot(dolar_compra, marker='x', linestyle='-', color='b',  label = "Dolar Comprador")    # Dibuja el gráfico
+plt.plot(dolar_venta, marker='x', linestyle='--', color='r',  label = "Dolar Vendedor")     # Dibuja el gráfico
+plt.grid(True)                      # Activa cuadrícula del gráfico pero no se muestra
+
+# EJE DE X abscisa
+indice = np.arange(5)               # Declara un array
+plt.xticks(indice, ("L", "M", "Mi", "J", "V")) 
+
 plt.legend()
-
 plt.show()
+

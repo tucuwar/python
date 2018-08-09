c8f4af29ab0e9efbe54d571917d9febfcddcaccd
tucuwar <tucuwar@gmail.com>
2018-08-09 12:35:27 -0300

    webs de referencia- sin cambios

--- mds_internos/internosmds.py
+++ mds_internos/internosmds.py
@@ -3,6 +3,19 @@
 # Agenda de internos MDS
 # ABM de internos
 
+# web referencia
+# https://python-para-impacientes.blogspot.com/2014/02/operaciones-con-archivos.html
+# http://chuwiki.chuidiang.org/index.php?title=Leer_y_escribir_ficheros_en_python
+
+
 
 import locale
 print(locale.getpreferredencoding())
+
+
+archivo = open("agenda.txt")
+
+archivo.write(cadena1 + '\n')
+
+
+archivo.close()

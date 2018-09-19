ca10cbb3fc05402d7fa30f9b96417d56d861f96f
tucuwar <tucuwar@gmail.com>
2018-09-19 10:03:33 -0300

    limpieza

--- python_varios/temperatura_FOR.py
+++
@@ -1,8 +0,0 @@
-print("Conversor Fahrenheit a Celsius con FOR")
-temp = int(input("Ingrese temperatura: "))
-print("F     C")
-for temp in range(temp,73,10):
-  print(temp,"  ",int((temp-32)*5/9))
-
-#in range (inicio, final-1, aumenta variable en)
-#
--- test_operaciones.py
+++
@@ -1,7 +0,0 @@
-CI = int(input("Capital Inicial: "))
-ti = int(input("Tasa de interes % "))
-P = int(input("Periodos a pagar (meses): "))
-
-a=round((ti%100),2)
-b=a/100
-print b

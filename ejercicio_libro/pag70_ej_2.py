7d6502f3840dd86a87497cfef12b62f7cd6fbc9d
tucuwar <tucuwar@gmail.com>
2018-09-20 12:20:21 -0300

    ejercicios pagina 70 enunciados

---
+++ ejercicio_libro/pag70_ej_2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#Curso Python para Principiantes - Eugenia Bahit
+
+# pagina 70
+# ejercicio 2
+#Crear un módulo para validación de contraseñas. Dicho módulo, deberá cumplir con
+#los siguientes criterios de aceptación:
+#La contraseña debe contener un mínimo de 8 caracteres
+#Una contraseña debe contener letras minúsculas, mayúsculas, números y al menos
+#1 carácter no alfanumérico
+#La contraseña no puede contener espacios en blanco
+#Contraseña válida, retorna True
+#Contraseña no válida, retorna el mensaje “La contraseña elegida no es segura”

71c65e8875dbe2d9845ceb8a36106d0f1ffc211a
tucuwar <tucuwar@gmail.com>
2018-09-26 16:33:47 -0300

    revisar variables booleanas

--- ejercicio_libro/pag70_ej_1.py
+++ ejercicio_libro/pag70_ej_1.py
@@ -17,15 +17,43 @@
 # Nombre de usuario válido, retorna True
 
 
-#def
+def validaUsr(usuario):
+    tam = len(usuario)
+    alfanum = usuario.isalnum()
+    
+    if tam >= 6: 
+        print ("tam mayor igual que 6 - OK")
+        if tam <= 12:
+            print ("tam menor igual que 12 - OK")
+            check1 = True
+        else: 
+            print ("El nombre de usuario no puede contener más de 12 caracteres")
+            chekck1 = False
+            #return
+    else: 
+        print ("El nombre de usuario debe contener al menos 6 caracteres")
+        return
+        
+    if alfanum is True:
+        print ("Cadena alfanumerica")
+        check2 = True
+    else: 
+        print ("El nombre de usuario puede contener solo letras y números")
+        check2= False
+        #return
+
+    valida = bool(check1) == bool(check2)
+    print valida
+    raw_input("Pulsa una tecla para continuar...sale")     
+        
+                
 
 print ("Ingrese usuario y contraseña")
 print (""" Recuerde:
         El nombre de usuario debe contener un mínimo de 6 caracteres y un máximo de 12
-        El nombre de usuario debe ser alfanumérico
+        El nombre de usuario puede ser alfanumérico
         """)
 
 usuario = raw_input("Usuario: ")
 password = raw_input("Contraseña: ")
-
-#valido login
+validaUsr(usuario)
